8f20090a7cf142eee304e20ceecce6ddd2f4b391
szenmode <83439414+s-zenmode@users.noreply.github.com>
2021-07-27 19:16:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Loading the spammer..")
+print(  )
+print(  )
+
+print("Welcome to the spammer.")
+print("To start the terminal hell, type in whatever you want in the terminal!")
+print("If you just wanna go back like the wuss you are, type in -exit.")
+
+
+print(  )
+print(  )
+print("What is the top line you would like to spam? (To exit say -exit)")
+spamString1 = input()
+print("What is the bottom line you would like to spam?")
+spamString2 = input()
+
+if spamString1 == "-exit":
+  exit("The kid's a wuss.")
+else:
+  while 3 > 2:
+    print(spamString1)
+    print(spamString2)
